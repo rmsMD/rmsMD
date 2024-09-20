e6cdfdd16495c42bd395909a16b50451ae886e2a
sjtingle <samjamestingle@gmail.com>
2024-09-20 11:08:30 +0100

    init

---
+++ R/summarymodel_ols_test.R
@@ -0,0 +1,18 @@
+#' Title of the function
+#'
+#' this is the short description
+#'
+#' @param arg blah
+#'
+#'
+#' @return some stuff
+#'
+#' @export
+#'
+#' @examples
+#' summarymodel_ols_test("eg")
+
+
+summarymodel_ols_test <- function(arg) {
+  print(paste(arg,"Hello, world!"))
+}
